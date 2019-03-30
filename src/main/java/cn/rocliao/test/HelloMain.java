01bd505b5d20e640ed112fc031f7184ecf6abc54
liaopeng <liaopeng2009@gmail.com>
2019-03-30 16:37:55 +0800

    my first use git

---
+++ src/main/java/cn/rocliao/test/HelloMain.java
@@ -0,0 +1,20 @@
+package cn.rocliao.test;
+
+/**
+ * Hello World
+ *
+ * @author liaopeng
+ * @date 2019-03-30
+ */
+public class HelloMain {
+
+    /**
+     * 主函数
+     *
+     * @param agrs 输入参数
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello " + (args.length > 0 ? args[0] : "Wolrd") + "!");
+
+    }
+}
